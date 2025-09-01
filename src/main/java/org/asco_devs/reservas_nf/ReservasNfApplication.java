ff165446be843055ea3a8a0f4304984de595174b
breyes396 <breyes-2024396@kinal.edu.gt>
2025-09-01 11:23:25 -0600

    feat: Agregar proyecto Spring inicial

---
+++ src/main/java/org/asco_devs/reservas_nf/ReservasNfApplication.java
@@ -0,0 +1,13 @@
+package org.asco_devs.reservas_nf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasNfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasNfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/asco_devs/reservas_nf/ReservasNfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asco_devs.reservas_nf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasNfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

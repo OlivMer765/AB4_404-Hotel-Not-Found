41a22d8fa5b43c6b51c969fb239fb96351219946
AlexGB12 <cguitz-2022434@kinal.edu.gt>
2025-09-02 09:15:06 -0600

    fix:renombrar clases

--- src/main/java/org/asco_devs/reservas_nf/service/HuespedService.java
+++ src/main/java/org/asco_devs/reservas_nf/service/HuespedService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class HuespedesService implements IHuespedesService {
+public class HuespedService implements IHuespedService {
 
     @Autowired
     private HuespedRepository huespedRepository;
--- src/main/java/org/asco_devs/reservas_nf/service/IHuespedService.java
+++ src/main/java/org/asco_devs/reservas_nf/service/IHuespedService.java
@@ -3,7 +3,7 @@ package org.asco_devs.reservas_nf.service;
 import org.asco_devs.reservas_nf.entity.Huesped;
 import java.util.List;
 
-public interface IHuespedesService {
+public interface IHuespedService {
     public List<Huesped> listarHuespedes();
     public Huesped buscarHuespedes(Integer idHuesped);
     public void guardarhuespedes(Huesped huesped);

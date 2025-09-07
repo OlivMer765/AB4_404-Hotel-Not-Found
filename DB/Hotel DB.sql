bbfc348176b391e71a70d90621252d51f5f7c867
eGalv3zDev <acamposeco-2024018@kinal.edu.gt>
2025-09-07 11:21:40 -0600

    T16 - Nueva Entidad Usuarios en la DB

--- DB/Hotel DB.sql
+++ DB/Hotel DB.sql
@@ -1,6 +1,16 @@
 create database HotelDB;
 use HotelDB;
 
+create table usuarios (
+    id bigint auto_increment primary key,
+    nombre varchar(64) not null,
+    apellido varchar(64) not null,
+    correo varchar(128) unique not null,
+    telefono varchar(8) not null,
+    direccion varchar(128) not null,
+    contrasena varchar(100) not null
+);
+
 create table Huespedes (
     idHuesped int auto_increment,
     nombre varchar(64),
